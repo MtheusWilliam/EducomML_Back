2999229e40f414ed40779ba072d89f53fa022cbd
Matheus William <matheuswg99@gmail.com>
2020-07-23 21:32:27 -0300

    Deploy da aplicação

--- EducomML_Back/EducomML_Back/settings.py
+++ EducomML_Back/EducomML_Back/settings.py
@@ -13,10 +13,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import datetime
 from corsheaders.defaults import default_headers
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
