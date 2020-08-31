a7cca2e811fb250984c28aaca74a1866bfba89e5
Matheus William <matheuswg99@gmail.com>
2020-08-31 20:03:33 -0300

    Update settings

--- EducomML_Back/settings.py
+++ EducomML_Back/settings.py
@@ -156,6 +156,15 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'X-CSRFTOKEN',
     'Access-Control-Allow-Origin',
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 EMAIL_USE_TLS = True
