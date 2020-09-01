6afdcd240dbf2eaa55ad3c3ca1133ea89c511cc8
Matheus William <matheuswg99@gmail.com>
2020-08-31 21:02:24 -0300

    Retirando Groups

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, get_user_model
 from .models import *
 from rest_framework import viewsets
