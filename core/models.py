0dce939b757402003a49ff73f9c6e7ada2e3130d
Matheus William <matheuswg99@gmail.com>
2020-08-31 21:21:19 -0300

    Reinserindo Groups

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User, AbstractUser, Group
 from django.db import models
 
 
