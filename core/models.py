cbdafd117e2688d7f38339728665ce241e424906
Matheus William <matheuswg99@gmail.com>
2020-08-31 21:06:55 -0300

    Delete group

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, Group, AbstractUser
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 
 
