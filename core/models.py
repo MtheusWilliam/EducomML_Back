c72b5a412e4f92566fdc64d61d3d0e247d75afba
Matheus William <matheuswg99@gmail.com>
2020-08-31 21:04:51 -0300

    Retirando typethreshold duplicado

--- core/models.py
+++ core/models.py
@@ -462,16 +462,7 @@ class Scopo(models.Model):
 
     class Meta:
         db_table = 'Scopo'
-
-
-class Typethreshold(models.Model):
-    # Field name made lowercase.
-    idtypethreshold = models.AutoField(
-        db_column='idTypeThreshold', primary_key=True)
-    # Field name made lowercase.
-    nametypethreshold = models.CharField(
-        db_column='nameTypeThreshold', max_length=256, blank=False, null=False)
-
+        
 class Typethreshold(models.Model):
     # Field name made lowercase.
     idtypethreshold = models.AutoField(
