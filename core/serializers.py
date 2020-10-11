cb1ab3d0b1217fec5ba13cde847f0aee2568aced
Vitor Azevedo <37710289+vitorAzevedo09@users.noreply.github.com>
2020-10-11 12:56:03 -0300

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -241,13 +241,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         user = User.objects.create_user(**validated_data)
         user.is_active = False
         subject = "Email de confirmação | Confirme seu cadastro na plataforma EducomML"
-        message = render_to_string('email_template.html', {
-            'user': user,
-            'domain': 'educomml-back.herokuapp.com',
-            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            'token': account_activation_token.make_token(user),
-        })
-        user.email_user(subject, message)
+        user.email_user(subject, "dsfasdfasdf")
         return user
 
     def update(self, instance, validated_data):
