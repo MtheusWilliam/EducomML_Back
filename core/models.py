e948824dbac711465e62ca6a374f8c0e2054dd94
Vitor Azevedo <37710289+vitorAzevedo09@users.noreply.github.com>
2020-10-11 09:10:08 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -7,6 +7,7 @@ from django.core.mail import EmailMessage
 class User(AbstractUser):
     description = models.CharField(max_length=100, default='')
     city = models.CharField(max_length=100, default='')
+    phoneNumber = models.IntegerField(default=0)
     image = models.ImageField(upload_to='profile_image', blank=True)
 
     def __str__(self):
